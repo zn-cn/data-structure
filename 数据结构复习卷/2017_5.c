2ae24a834af77c27e1501a2e4a32aebbbad7f784
tofar <2205567926@qq.com>
2018-01-21 23:49:32 +0800

    hh

--- 数据结构复习卷/2017_5.c
+++ 数据结构复习卷/2017_5.c
@@ -5,8 +5,10 @@ int get_last_two_num(int k, int n)
 {
 	if (n == 1)
 		return k % 100;
+	else if (n % 2 == 0)
+		return get_last_two_num((k * k) % 100, n / 2) % 100;
 	else
-		return get_last_two_num(k, n / 2) * get_last_two_num(k, n - n / 2) % 100;
+		return get_last_two_num((k * k) % 100, n / 2) * k % 100;
 }
 // 获取十位数
 int get_tens_digit(int k, int n)
