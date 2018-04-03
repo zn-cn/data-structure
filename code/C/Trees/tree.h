1ad0a5584150d6c5b96567b7620683a2f209e0b0
tofar <2205567926@qq.com>
2018-04-03 22:45:24 +0800

    add Btree

--- code/C/Trees/tree.h
+++ code/C/Trees/tree.h
@@ -8,14 +8,14 @@ struct TreeNode;
 typedef struct TreeNode *Position;
 typedef struct TreeNode *SearchTree;
 
-SearchTree MakeEmpty( SearchTree T );
-Position Find( ElementType X, SearchTree T );
-Position FindMin( SearchTree T );
-Position FindMax( SearchTree T );
-SearchTree Insert( ElementType X, SearchTree T );
-SearchTree Delete( ElementType X, SearchTree T );
-ElementType Retrieve( Position P );
+SearchTree MakeEmpty(SearchTree T);
+Position Find(ElementType X, SearchTree T);
+Position FindMin(SearchTree T);
+Position FindMax(SearchTree T);
+SearchTree Insert(ElementType X, SearchTree T);
+SearchTree Delete(ElementType X, SearchTree T);
+ElementType Retrieve(Position P);
 
-#endif  /* _Tree_H */
+#endif /* _Tree_H */
 
 /* END */
